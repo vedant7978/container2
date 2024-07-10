56e3cfa935e4e07edefb69d65c7f56093ea35089
Vedant <vd785266@dal.ca>
2024-07-09 22:52:09 -0300

    test 2

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const FILE_DIRECTORY = process.env.FILE_DIRECTORY || "../";
 
 console.log({ FILE_DIRECTORY })
 console.log({ PORT })
-console.log("cicd testing.....")
+console.log("cicd testing.............")
 
 
 app.post("/parser", (req, res) => {
